a7b7c8eb3e4eca46901c291c68c129266e8952f1
chris98122 <chris98122@126.com>
2020-06-06 14:08:14 +0800

    use pagerank

---
+++ GRAPHX/src/main/scala/wc/Pagerank.scala
@@ -0,0 +1,25 @@
+package wc
+
+import org.apache.spark.graphx._
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Pagerank {
+  def main(args: Array[String]) {
+    /**
+     * SparkContext 的初始化需要一个SparkConf对象
+     * SparkConf包含了Spark集群的配置的各种参数
+     */
+    val conf = new SparkConf()
+      .setMaster("local") //启动本地化计算
+      .setAppName("testRdd") //设置本程序名称
+
+    //Spark程序的编写都是从SparkContext开始的
+    val sc = new SparkContext(conf)
+    // Load the edges as a graph
+    val graph = GraphLoader.edgeListFile(sc, ".//file//Wiki-Vote.txt")
+    // Run PageRank
+    val ranks = graph.pageRank(0.0001).vertices
+    // Print the result
+    println(ranks.collect().mkString("\n"))
+  }
+}
